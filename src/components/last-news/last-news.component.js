2fc02cc2ad3720655d05f182bb59afdc8c9557b3
Bekzod <bekzodisakov18@gmail.com>
2022-06-21 16:10:57 +0500

    fix: last-news component

--- src/components/last-news/last-news.component.js
+++ src/components/last-news/last-news.component.js
@@ -10,37 +10,22 @@ import {
   LastNewsTitle
 } from './last-news.style';
 import { hoc } from '@utils/hoc';
-import { config } from 'src/config';
 
-export const LastNews = hoc(
-  useNewsLastProps,
-  ({ latestNews, day, ...rest }) => {
-    return (
-      <LastNewsSpace {...rest}>
-        <LastNewsContainer
-          lastNewsBanner={latestNews?.banner?.url}
-          href={`${config.landingUrl}/blog/${latestNews?.slug}`}
-          target='_blank'
-        >
-          <LastNewsTitle data-testid='last-news-title'>
-            {latestNews?.title?.uz}
-          </LastNewsTitle>
-          <LastNewsAuthorWrapper>
-            <LastNewsAuthorImg
-              src={latestNews?.thumbnail?.url}
-              alt={latestNews?.title?.uz}
-              width={40}
-              height={40}
-            />
-            <LastNewsBottomWrapper>
-              <LastNewAuthorName data-testid='last-news-author'>
-                {latestNews?.author?.uz}
-              </LastNewAuthorName>
-              <LastNewsDate data-testid='last-news-day'>{day}</LastNewsDate>
-            </LastNewsBottomWrapper>
-          </LastNewsAuthorWrapper>
-        </LastNewsContainer>
-      </LastNewsSpace>
-    );
-  }
-);
+export const LastNews = hoc(useNewsLastProps, ({ ...rest }) => {
+  return (
+    <LastNewsSpace {...rest}>
+      <LastNewsContainer lastNewsBanner={''} href={``} target='_blank'>
+        <LastNewsTitle data-testid='last-news-title'>{''}</LastNewsTitle>
+        <LastNewsAuthorWrapper>
+          <LastNewsAuthorImg src={''} alt={''} width={40} height={40} />
+          <LastNewsBottomWrapper>
+            <LastNewAuthorName data-testid='last-news-author'>
+              {''}
+            </LastNewAuthorName>
+            <LastNewsDate data-testid='last-news-day'>{''}</LastNewsDate>
+          </LastNewsBottomWrapper>
+        </LastNewsAuthorWrapper>
+      </LastNewsContainer>
+    </LastNewsSpace>
+  );
+});
