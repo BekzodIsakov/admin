ea881945e5f69abb10ce46969726831b3d497129
Bekzod <bekzodisakov18@gmail.com>
2022-06-21 15:15:02 +0500

    fix: minimize services & store

--- src/services/common.service.js
+++ src/services/common.service.js
@@ -7,20 +7,12 @@ class CommonService {
     this.storageService = storageService;
   }
 
-  getLatestNews = () => this.api.get('/post-latest');
-
-  recommendedCourses = () => this.api.get('/recomended-course');
-
   getProfile = () => {
     if (!this.storageService.getAccessToken()) {
       return Promise.reject(new Error('Unauthorized'));
     }
     return this.api.get('/student');
   };
-
-  promoCourses = () => this.api.get('/promo');
-
-  uploadFile = (folder, data) => this.api.post('/upload/' + folder, data);
 }
 
 export const commonService = new CommonService(api, storageService);
